dc9550e7c039c49c9c71fd93c46ac82538f72fb7
Luiss Resources <luissresources@gmail.com>
2022-08-20 13:42:17 -0500

    build: new release

---
+++ src/App.js
@@ -0,0 +1,172 @@
+import React, {useState, useEffect} from 'react'
+import Footer from './components/Footer'
+import LoginForm from './components/forms/LoginForm'
+import NoteForm from './components/forms/NoteForm'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import loginService from './services/login'
+import noteService from './services/notes'
+
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response)
+      })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+  
+  const handleNoteChange = e => setNewNote(e.target.value)
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    noteService 
+    .create(noteObject)
+      .then(returnedNote => {
+        // setNotes(notes.concat(returnedNote.data))
+        noteService
+        .getAll()
+        .then(response => {
+          setNotes(response)
+        })
+        setShowAll(false)
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(note.id, changedNote)
+        .then(returnedNote => {
+          noteService
+            .getAll()
+              .then(notes => {
+                setNotes(notes)
+              })
+          })
+        .catch(error => {
+          setErrorMessage(
+            `Note '${note.content}' was already removed from server`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setNotes(notes.filter(n => n.id !== id))
+        })
+  }
+
+  const filter = notes.filter(note => {
+    return (
+      showAll 
+      ? note.important === showAll
+      : note.important === true || note.important === false
+    )
+  })
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  return (
+    <>
+      <h1>Notes</h1>
+      <Notification 
+        message={errorMessage}
+      />
+      { user === null 
+        ? 
+          <Togglable buttonLabel='login'>
+            <LoginForm
+              handleLogin= { handleLogin }
+              username = { username }
+              setUsername = { setUsername }
+              password = { password }
+              setPassword = { setPassword }
+            />
+          </Togglable>
+        : 
+          <div>
+            <p>{user.name} logged in</p>
+            <div>
+              <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                  show {!showAll ? 'important' : 'all' }
+                </button>
+              </div>      
+              <ul>
+                {
+                  <>
+                    {filter.map((note, i) => 
+                      <Note
+                        key={i}
+                        note={note} 
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                      />
+                    )} 
+                  </>
+                }
+              </ul>
+            </div>
+            <Togglable buttonLabel='new note'>
+              <NoteForm 
+                addNote = { addNote }
+                newNote = { newNote }
+                handleNoteChange = { handleNoteChange }
+              />
+            </Togglable>
+          </div>
+      }
+      <Footer />
+    </>
+  )
+} 
+
+export default App;
---
+++ src/services/notes.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request
+    .then(response => response.data)
+}
+
+const create = async newObject => {
+  // return axios.post(baseUrl, newObject)
+
+  const config = {
+    headers: { Authorization : token}
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+const noteService = { getAll, create, update, setToken }
+
+export default noteService;
